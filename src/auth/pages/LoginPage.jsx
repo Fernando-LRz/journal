3085c6ead146cbb6023780d4d7af4a5227507e91
Fernando LR <fernando.lorenzo1899@gmail.com>
2024-01-15 15:22:16 -0600

    add AuthLayout

--- src/auth/pages/LoginPage.jsx
+++ src/auth/pages/LoginPage.jsx
@@ -2,70 +2,54 @@ import { Link as RouterLink } from 'react-router-dom';
 import { Google } from '@mui/icons-material';
 import { Grid, Typography, TextField, Button, Link } from '@mui/material';
 
+import { AuthLayout } from '../layout/AuthLayout';
+
 export const LoginPage = () => {
     return (
-        <Grid
-            container
-            spacing={ 0 }
-            direction="column"
-            alignItems="center"
-            justifyContent="center"
-            sx={{ minHeight: "100vh", backgroundColor: "primary.main", padding: 4 }}
-        >
+        <AuthLayout title="Inicio de sesión">
+            <form>
+                <Grid container>
 
-            <Grid
-                item
-                className="box-shadow"   
-                xs={ 3 }
-                sx={{ backgroundColor: "#FFF", padding: 3, borderRadius: 2 }} 
-            >
-                <Typography variant="h5" sx={{ mb: 1 }}>Inicio de sesión</Typography>
-
-                <form>
-                    <Grid container>
-
-                        <Grid item xs={ 12 } md={ 6 } sx={{ mt: 2 }}>
-                            <TextField 
-                                label="Correo electrónico" 
-                                type="email" 
-                                placeholder="Ingresa tu correo electrónico"
-                                fullWidth
-                            />
-                        </Grid>
-
-                        <Grid item xs={ 12 } md={ 6 } sx={{ mt: 2 }}>
-                            <TextField 
-                                label="Contraseña" 
-                                type="password" 
-                                placeholder="Ingresa una contraseña"
-                                fullWidth
-                            />
-                        </Grid>
-                        
-                        <Grid container spacing={ 2 } sx={{ mb: 2, mt: 1 }}>
-                            <Grid item xs={ 12 } sm={ 6 }>
-                                <Button variant="contained" fullWidth>
-                                    Iniciar sesión
-                                </Button>
-                            </Grid>
-                            <Grid item xs={ 12 } sm={ 6 }>
-                                <Button variant="contained" fullWidth>
-                                    <Google />
-                                    <Typography sx={{ ml: 1 }}>Google</Typography>
-                                </Button>
-                            </Grid>
-                        </Grid>
-
-                        <Grid container direction="row" justifyContent="end">
-                            <Link component={ RouterLink } color="inherit" to="/auth/register">
-                                Crear cuenta
-                            </Link>
-                        </Grid>
-
+                    <Grid item xs={ 12 } md={ 6 } sx={{ mt: 2 }}>
+                        <TextField 
+                            label="Correo electrónico" 
+                            type="email" 
+                            placeholder="Ingresa tu correo electrónico"
+                            fullWidth
+                        />
                     </Grid>
-                </form>
-            </Grid>
 
-        </Grid>
+                    <Grid item xs={ 12 } md={ 6 } sx={{ mt: 2 }}>
+                        <TextField 
+                            label="Contraseña" 
+                            type="password" 
+                            placeholder="Ingresa una contraseña"
+                            fullWidth
+                        />
+                    </Grid>
+                    
+                    <Grid container spacing={ 2 } sx={{ mb: 2, mt: 1 }}>
+                        <Grid item xs={ 12 } sm={ 6 }>
+                            <Button variant="contained" fullWidth>
+                                Iniciar sesión
+                            </Button>
+                        </Grid>
+                        <Grid item xs={ 12 } sm={ 6 }>
+                            <Button variant="contained" fullWidth>
+                                <Google />
+                                <Typography sx={{ ml: 1 }}>Google</Typography>
+                            </Button>
+                        </Grid>
+                    </Grid>
+
+                    <Grid container direction="row" justifyContent="end">
+                        <Link component={ RouterLink } color="inherit" to="/auth/register">
+                            Crear cuenta
+                        </Link>
+                    </Grid>
+
+                </Grid>
+            </form>
+        </AuthLayout>
     );
 };

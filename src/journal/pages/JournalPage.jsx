df8c32821f707ff29ea50c71229106b239977255
Fernando LR <fernando.lorenzo1899@gmail.com>
2024-01-21 13:12:20 -0600

    add a floating action button

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,4 +1,6 @@
-import { Typography } from '@mui/material';
+import { IconButton, Typography } from '@mui/material';
+import { AddOutlined } from '@mui/icons-material';
+
 import { JournalLayout } from '../layout/JournalLayout';
 import { NoteView, NothingSelectedView } from '../views';
 
@@ -6,8 +8,22 @@ export const JournalPage = () => {
     return (
         <JournalLayout>
             {/* <Typography>Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui quisquam inventore provident ipsum? Illo harum expedita placeat, dignissimos iste, adipisci architecto corporis beatae quasi perspiciatis dolorum possimus nemo tempora fuga?</Typography>     */}
-            {/* <NothingSelectedView /> */}
-            <NoteView />
+            <NothingSelectedView />
+            {/* <NoteView /> */}
+
+            <IconButton
+                size="large"
+                sx={{
+                    color: "#FFF",
+                    backgroundColor: "error.main",
+                    ':hover': { backgroundColor: "error.main", opacity: 0.9 },
+                    position: "fixed",
+                    right: 50,
+                    bottom: 50
+                }}
+            >
+                <AddOutlined sx={{ fontSize: 30 }}/>
+            </IconButton>
         </JournalLayout>
     );
 };

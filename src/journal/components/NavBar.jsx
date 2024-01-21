4717c318bfd64e645221b8b8cf7626150764f00a
Fernando LR <fernando.lorenzo1899@gmail.com>
2024-01-21 12:22:24 -0600

    add SideBar

--- src/journal/components/NavBar.jsx
+++ src/journal/components/NavBar.jsx
@@ -20,7 +20,7 @@ export const NavBar = ({ drawerWidth = 240 }) => {
                 </IconButton>
 
                 <Grid container direction="row" justifyContent="space-between" alignItems="center">
-                    <Typography variant="h6" noWrap component='div'>JournalApp</Typography>
+                    <Typography variant="h6" noWrap component="div">JournalApp</Typography>
 
                     <IconButton color="error">
                         <LogoutOutlined />
---
+++ src/journal/components/SideBar.jsx
@@ -0,0 +1,47 @@
+import { Box, Divider, Drawer, Grid, List, ListItem, ListItemButton, ListItemIcon, ListItemText, Toolbar, Typography } from '@mui/material';
+import { TurnedInNot } from '@mui/icons-material';
+
+export const SideBar = ({ drawerWidth }) => {
+    return (
+        <Box
+            component="nav"
+            sx={{ width: { sm: drawerWidth }, flexShrink: { sm: 0 } }}
+        >
+            <Drawer
+                open
+                variant="permanent" // temporary
+                sx={{
+                    display: { xs: 'block' },
+                    '& .MuiDrawer-paper': { boxSizing: 'border-box', width: drawerWidth }
+                }}
+            >
+                <Toolbar>
+                    <Typography variant="h6" noWrap component='div'>
+                        Fernando LR
+                    </Typography>
+                </Toolbar>
+
+                <Divider />
+
+                <List>
+                    {
+                        ['Enero', 'Febrero', 'Marzo', 'Abril'].map( text => (
+                            <ListItem key={ text } disablePadding>
+                                <ListItemButton>
+                                    <ListItemIcon>
+                                        <TurnedInNot />
+                                    </ListItemIcon>
+
+                                    <Grid container>
+                                        <ListItemText primary={ text } />
+                                        <ListItemText secondary={ '' } />
+                                    </Grid>
+                                </ListItemButton>
+                            </ListItem>
+                        ))
+                    }
+                </List>
+            </Drawer>
+        </Box>
+    );
+};

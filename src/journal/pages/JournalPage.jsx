69e9ff9472eea4c2e2ac38abdb2899385439bc88
Fernando LR <fernando.lorenzo1899@gmail.com>
2024-01-21 12:47:18 -0600

    add NoteView

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,13 +1,13 @@
 import { Typography } from '@mui/material';
 import { JournalLayout } from '../layout/JournalLayout';
-import { NothingSelectedView } from '../views';
+import { NoteView, NothingSelectedView } from '../views';
 
 export const JournalPage = () => {
     return (
         <JournalLayout>
             {/* <Typography>Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui quisquam inventore provident ipsum? Illo harum expedita placeat, dignissimos iste, adipisci architecto corporis beatae quasi perspiciatis dolorum possimus nemo tempora fuga?</Typography>     */}
-        
-            <NothingSelectedView />
+            {/* <NothingSelectedView /> */}
+            <NoteView />
         </JournalLayout>
     );
 };
